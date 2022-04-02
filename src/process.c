6a8b0518217d55cc225d8a3131888fb8ed99db8a
Brunio25 <brunofpsoares25@gmail.com>
2022-04-02 21:20:18 +0100

    process.c completed

--- src/process.c
+++ src/process.c
@@ -1,28 +1,74 @@
-#include "memory.h"
+#include <stdio.h>
+#include <sys/wait.h>
+
+#include "client.h"
+#include "driver.h"
 #include "main.h"
+#include "memory.h"
+#include "restaurant.h"
 
 /* Função que inicia um novo processo restaurante através da função fork do SO. O novo
-* processo irá executar a função execute_restaurant respetiva, fazendo exit do retorno.
-* O processo pai devolve o pid do processo criado.
-*/
-int launch_restaurant(int restaurant_id, struct communication_buffers* buffers, struct main_data* data);
+ * processo irá executar a função execute_restaurant respetiva, fazendo exit do retorno.
+ * O processo pai devolve o pid do processo criado.
+ */
+int launch_restaurant(int restaurant_id, struct communication_buffers* buffers, struct main_data* data) {
+    int res;
 
+    int pid = fork();
+    if (pid == -1) {
+        exit(1);
+    } else if (pid == 0) {
+        exit(execute_restaurant(restaurant_id, buffers, data));
+    } else {
+        res = wait_process(pid);
+    }
+
+    return res;
+}
 
 /* Função que inicia um novo processo motorista através da função fork do SO. O novo
-* processo irá executar a função execute_driver, fazendo exit do retorno.
-* O processo pai devolve o pid do processo criado.
-*/
-int launch_driver(int driver_id, struct communication_buffers* buffers, struct main_data* data);
+ * processo irá executar a função execute_driver, fazendo exit do retorno.
+ * O processo pai devolve o pid do processo criado.
+ */
+int launch_driver(int driver_id, struct communication_buffers* buffers, struct main_data* data) {
+    int res;
 
+    int pid = fork();
+    if (pid == -1) {
+        exit(1);
+    } else if (pid == 0) {
+        exit(execute_driver(driver_id, buffers, data));
+    } else {
+        res = wait_process(pid);
+    }
+
+    return res;
+}
 
 /* Função que inicia um novo processo cliente através da função fork do SO. O novo
-* processo irá executar a função execute_client, fazendo exit do retorno.
-* O processo pai devolve o pid do processo criado.
-*/
-int launch_client(int client_id, struct communication_buffers* buffers, struct main_data* data);
+ * processo irá executar a função execute_client, fazendo exit do retorno.
+ * O processo pai devolve o pid do processo criado.
+ */
+int launch_client(int client_id, struct communication_buffers* buffers, struct main_data* data) {
+    int res;
 
+    int pid = fork();
+    if (pid == -1) {
+        exit(1);
+    } else if (pid == 0) {
+        exit(execute_client(client_id, buffers, data));
+    } else {
+        res = wait_process(pid);
+    }
 
-/* Função que espera que um processo termine através da função waitpid. 
-* Devolve o retorno do processo, se este tiver terminado normalmente.
-*/
-int wait_process(int process_id);
+    return res;
+}
+
+/* Função que espera que um processo termine através da função waitpid.
+ * Devolve o retorno do processo, se este tiver terminado normalmente.
+ */
+int wait_process(int process_id) {
+    int res;
+    waitpid(process_id, &res, 0);  // TODO check if this is the correct implementation
+    return res;
+}
